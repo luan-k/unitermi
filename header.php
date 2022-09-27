a2e60fada7bf9dbebcb9c038525587bf8ef093d1
luankds <luan@widgilabs.com>
2022-09-27 06:01:19 -0300

    bruh

--- header.php
+++ header.php
@@ -10,9 +10,9 @@
 <body <?php body_class(); ?> >
 			<div class="wraper" >
         <header class="header">
-            <nav>
+            <nav class="header-nav">
                 <div class="bg-unitermi-primary-redDark max-w-full logo-nav">
-                    <div class="flex justify-center md:justify-between">
+                    <div class="flex justify-center md:justify-between logo-nav-inner">
 
                         <!-- logo -->
                         <div class="logo-wraper">
@@ -37,30 +37,30 @@
                     <input class="side-menu" type="checkbox" id="side-menu"/>
                     <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
                     <div class="nav-menu-list" >
-                    <!-- <ul class="container relative flex items-center"> -->
-                        <!-- <li  class="btn-nav mobile-nav-logic <?php if (is_page('inicio')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/inicio')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Home </a></li>
-                        <li class="btn-nav mobile-nav-logic <?php if (is_page('produtos') OR get_post_type() == 'post') echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/produtos')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Produtos </a></li>
-                        <li class="btn-nav mobile-nav-logic <?php if (is_page('quem-somos')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/quem-somos')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Quem Somos </a></li>
-                        <li class="btn-nav mobile-nav-logic <?php if (is_page('servico') OR get_post_type() == 'servico') echo' current-menu-item' ?>"><a target="_blank" href="<?php echo esc_url(site_url('/wp-content/uploads/2022/09/Catalogo-reduzido.pdf')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Catalogo </a></li>
-                        <li class="btn-nav mobile-nav-logic <?php if (is_page('contato')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/contato')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Contato </a></li>
+						<!-- <ul class="container relative flex items-center"> -->
+							<!-- <li  class="btn-nav mobile-nav-logic <?php if (is_page('inicio')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/inicio')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Home </a></li>
+							<li class="btn-nav mobile-nav-logic <?php if (is_page('produtos') OR get_post_type() == 'post') echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/produtos')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Produtos </a></li>
+							<li class="btn-nav mobile-nav-logic <?php if (is_page('quem-somos')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/quem-somos')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Quem Somos </a></li>
+							<li class="btn-nav mobile-nav-logic <?php if (is_page('servico') OR get_post_type() == 'servico') echo' current-menu-item' ?>"><a target="_blank" href="<?php echo esc_url(site_url('/wp-content/uploads/2022/09/Catalogo-reduzido.pdf')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Catalogo </a></li>
+							<li class="btn-nav mobile-nav-logic <?php if (is_page('contato')) echo' current-menu-item' ?>"><a href="<?php echo esc_url(site_url('/contato')); ?>" class=" py-5 px-6 text-white md:text-gray-600 hover:text-white transition duration-300">Contato </a></li>
 
-                        <li class="wrapper  mobile-nav-logic-toogle block md:hidden">
-                            <a >
-                                <i class="fa fa-bars text-black"></i>
-                            </a>
-                        </li> -->
-                        <?php
+							<li class="wrapper  mobile-nav-logic-toogle block md:hidden">
+								<a >
+									<i class="fa fa-bars text-black"></i>
+								</a>
+							</li> -->
+							<?php
 
-                            wp_nav_menu( array(
-                                'theme_location'    => 'main_menu',
-                                'container'         => 'div',
-                                'container_class'   => 'menu-container',
-                                /* 'container_id'      => 'bs-example-navbar-collapse-1', */
-                                'menu_class'        => 'nav navbar-nav',
+								wp_nav_menu( array(
+									'theme_location'    => 'main_menu',
+									'container'         => 'div',
+									'container_class'   => 'menu-container',
+									/* 'container_id'      => 'bs-example-navbar-collapse-1', */
+									'menu_class'        => 'nav navbar-nav',
 
-                            ) );
+								) );
                         ?>
-                </div>
+                	</div>
                     <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger nav-item nav-link"><i class="fa fa-search" aria-hidden="true"></i></a>
                 </div>
 
